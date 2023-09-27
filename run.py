751414c9e192ec03dba5762e6523fc1e46217d2d
Studentofcodeinstitute <>
2023-09-27 18:05:26 +0000

    Create a function to print input values

--- run.py
+++ run.py
@@ -1,3 +1,12 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+game_state = [None, None, None, None, None, None, None, None, None]
+turn = 1
+
+def print_board():
+    global game_state
+    for i in [0, 1, 2]:
+        value_1 = game_state[i*3] if game_state[i*3] else " "
+        value_2 = game_state[i*3+1] if game_state[i*3+1] else " "
+        value_3 = game_state[i*3+2] if game_state[i*3+2] else " "
+        print(f"{value_1} | {value_2} | {value_3} ")
+        if i < 2:
+            print(f"--|---|---")
